9edbf561bc00de1ef8d4d6115bebaec088406853
lclbm <614867321@qq.com>
2022-07-27 02:01:01 +0800

    [update] 更新登录profile显示相关逻辑

--- static/index.js
+++ static/index.js
@@ -27,6 +27,14 @@ async function search_player(name) {
 
 window.onload = async function () {
 
+    $("#search_button").click(function () {
+        var name = $("#search_input").val();
+        search_player(name);
+    });
+
+    $("#search_input").on('keypress', function (e) { if (e.keyCode == 13) { $("#search_button").click(); } });
+
+    
     var codeReg = /^\?code=(.{32})$/;
     var code = codeReg.exec(window.location.search);
     if (code) {
@@ -38,17 +46,29 @@ window.onload = async function () {
 
     var access_token = Cookies.get('access_token');
     var refresh_token = Cookies.get('refresh_token');
+    var membershipData;
 
-    if (refresh_token) {
-        if (!access_token) {
-            let token = await API.refresh_token(refresh_token);
-            Cookies.set('access_token', token.access_token, { expires: token.expires_in / 3600 });
-            Cookies.set('refresh_token', token.refresh_token, { expires: token.refresh_expires_in / 3600 });
+    try {
+        if (refresh_token) {
+            // 如果refresh_token存在且未过期，access_token已过期，则刷新access_token
+            if (!access_token) {
+                let token = await API.refresh_token(refresh_token);
+                Cookies.set('access_token', token.access_token, { expires: token.expires_in / 3600 });
+                Cookies.set('refresh_token', token.refresh_token, { expires: token.refresh_expires_in / 3600 });
+            }
+            access_token = Cookies.get('access_token');
+            membershipData = await API.fetch_membershipdata_for_current_user(access_token);
+            console.log(membershipData);
+
         }
-        access_token = Cookies.get('access_token');
-        var membershipData = await API.fetch_membershipdata_for_current_user(access_token);
-        console.log(membershipData);
+    } catch (error) {
+        Cookies.remove('access_token');
+        Cookies.remove('refresh_token');
+    }
 
+    if (membershipData) {
+        // $("#loginPlaceHolder").html(`<a class="btn btn-outline-primary" href="https://www.bungie.net/zh-chs/oauth/authorize?client_id=40835&response_type=code" role="button"
+        // style="width: 8rem;font-size:1.2rem">登录</a>`);
     } else {
         $("#loginPlaceHolder").html(`<a class="btn btn-outline-primary" href="https://www.bungie.net/zh-chs/oauth/authorize?client_id=40835&response_type=code" role="button"
         style="width: 8rem;font-size:1.2rem">登录</a>`);
@@ -56,9 +76,4 @@ window.onload = async function () {
 
 
 
-    $("#search_button").click(function () {
-        var name = $("#search_input").val();
-        search_player(name);
-    });
-    $("#search_input").on('keypress', function (e) { if (e.keyCode == 13) { $("#search_button").click(); } });
 };
